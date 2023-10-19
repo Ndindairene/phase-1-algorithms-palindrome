b4c94890a0e262fc28ad5c7934283bdcaf12c401
Ndindairene <irene.ndindaa@gmail.com>
2023-10-19 16:15:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+
+function reverseString(word){
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+
+  return reversedWord;
+}
+
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here)
+const reversedWord = reverseString(word);
+ 
+if (word === reversedWord) {
+  return true;
+}else {
+  return false;
+}
 }
 
 /* 
   Add your pseudocode here
+  if the input reads the same in reverse order,
+  return true
+  else return false
+  
 */
 
 /*
   Add written explanation of your solution here
+  The function should return true if the test reads same
+  forward and backward and false if not.
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,6 +41,27 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
